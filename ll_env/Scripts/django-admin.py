00c9cbcf24ae0e478e6acf19c888709c43dd12f7
Zachary <courage0322@gmail.com>
2019-03-28 15:08:07 +0800

    create django

---
+++ ll_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\zachary\documents\github\login_by_face_recognition\ll_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
