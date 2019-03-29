3432db25effa41f7d089bf3a932c4a56062d9d3e
Zachary <courage0322@gmail.com>
2019-03-29 14:48:13 +0800

    [add] 建立post comment 模型

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from blogs.models import Post, Comment
+
+admin.site.register(Post)
+admin.site.register(Comment)
+
 # Register your models here.
--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    """A post is posted by a user"""
+    title = models.CharField(max_length=20)
+    content = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    # owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.title
+
+
+class Comment(models.Model):
+    """Someone comment on a post."""
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'comments'
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.text
