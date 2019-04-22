22030e76fbd0932defbe034a628ad306035b1499
zacharyzhuo <47736453+zacharyzhuo@users.noreply.github.com>
2019-04-23 00:29:39 +0800

    [add] test webRTC

--- ll_env/Scripts/django-admin.py
+++ ll_env/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!c:\users\zachary\documents\github\login_by_face_recognition\ll_env\scripts\python.exe
+#!c:\users\chen\documents\github\login_by_face_recognition\ll_env\scripts\python.exe
 from django.core import management
 
 if __name__ == "__main__":
